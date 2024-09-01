d648d692a7f437471f77153e90729246d1c7c694
Adam <papisadamt@gmail.com>
2024-09-01 16:01:15 +0200

    Add adding, editing, and deleting of appointments in React state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
 import Paper from "@mui/material/Paper";
-import { ViewState } from "@devexpress/dx-react-scheduler";
+import { ViewState, EditingState } from "@devexpress/dx-react-scheduler";
 import {
   Scheduler,
   WeekView,
@@ -11,11 +12,15 @@ import {
   MonthView,
   DayView,
   AppointmentTooltip,
+  AppointmentForm,
+  EditRecurrenceMenu,
+  AllDayPanel,
+  ConfirmationDialog,
 } from "@devexpress/dx-react-scheduler-material-ui";
 import "./App.css";
 import { appointments } from "./data/appointments";
-import { useState } from "react";
 import Appointment from "./Appointment/Appointment";
+
 function App() {
   const [data, setData] = useState(appointments);
   const [currentDate, setCurrentDate] = useState("2018-06-27");
@@ -24,6 +29,9 @@ function App() {
     target: null,
     data: {},
   });
+  const [addedAppointment, setAddedAppointment] = useState({});
+  const [appointmentChanges, setAppointmentChanges] = useState({});
+  const [editingAppointment, setEditingAppointment] = useState(undefined);
 
   const currentDateChange = (newDate) => {
     setCurrentDate(newDate);
@@ -37,6 +45,40 @@ function App() {
     setAppointmentMeta({ data, target });
   };
 
+  const changeAddedAppointment = (addedAppointment) => {
+    setAddedAppointment(addedAppointment);
+  };
+
+  const changeAppointmentChanges = (appointmentChanges) => {
+    setAppointmentChanges(appointmentChanges);
+  };
+
+  const changeEditingAppointment = (editingAppointment) => {
+    setEditingAppointment(editingAppointment);
+  };
+
+  const commitChanges = ({ added, changed, deleted }) => {
+    setData((prevData) => {
+      let newData = [...prevData];
+      if (added) {
+        const startingAddedId =
+          newData.length > 0 ? newData[newData.length - 1].id + 1 : 0;
+        newData = [...newData, { id: startingAddedId, ...added }];
+      }
+      if (changed) {
+        newData = newData.map((appointment) =>
+          changed[appointment.id]
+            ? { ...appointment, ...changed[appointment.id] }
+            : appointment
+        );
+      }
+      if (deleted !== undefined) {
+        newData = newData.filter((appointment) => appointment.id !== deleted);
+      }
+      return newData;
+    });
+  };
+
   return (
     <Paper>
       <Scheduler data={data} height={700}>
@@ -44,9 +86,21 @@ function App() {
           currentDate={currentDate}
           onCurrentDateChange={currentDateChange}
         />
+        <EditingState
+          onCommitChanges={commitChanges}
+          addedAppointment={addedAppointment}
+          onAddedAppointmentChange={changeAddedAppointment}
+          appointmentChanges={appointmentChanges}
+          onAppointmentChangesChange={changeAppointmentChanges}
+          editingAppointment={editingAppointment}
+          onEditingAppointmentChange={changeEditingAppointment}
+        />
         <MonthView />
         <WeekView startDayHour={9} endDayHour={19} />
         <DayView />
+        <AllDayPanel />
+        <EditRecurrenceMenu />
+        <ConfirmationDialog />
         <Toolbar />
         <DateNavigator />
         <TodayButton />
@@ -65,7 +119,10 @@ function App() {
           onVisibilityChange={toggleVisibility}
           appointmentMeta={appointmentMeta}
           onAppointmentMetaChange={onAppointmentMetaChange}
+          showOpenButton
+          showDeleteButton
         />
+        <AppointmentForm />
         <ViewSwitcher />
       </Scheduler>
     </Paper>
